866fd0be3a39f2d2256be3519ca0a54fd1ae9aed
SoHyeon Kim <happythgus89@gmail.com>
2022-02-07 10:45:19 +0900

    Variables

--- Week1/NewBehaviourScript.cs
+++ Week1/NewBehaviourScript.cs
@@ -8,5 +8,24 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello Unity!");
+
+        // 1. 변수
+
+        int level = 5;
+        float strength = 15.5f;
+        string playerName = "브로콜리";
+        bool isFullLevel = false;
+
+        /*
+        Debug.Log("용사의 이름은?");
+        Debug.Log(playerName);
+        Debug.Log("용사의 레벨은?");
+        Debug.Log(level);
+        Debug.Log("용사의 힘은?");
+        Debug.Log(strength);
+        Debug.Log("용사는 만렙인가?");
+        Debug.Log(isFullLevel);
+        */
+
     }
 }
