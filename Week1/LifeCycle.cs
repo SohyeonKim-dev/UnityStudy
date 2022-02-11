1cc796ebb1e29c7db5f8df50125a22efd4402d62
SoHyeon Kim <happythgus89@gmail.com>
2022-02-11 15:37:01 +0900

    LifeCycle

---
+++ Week1/LifeCycle.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeCycle : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("플레이어 데이터가 준비되었습니다. ")
+    }
+}
