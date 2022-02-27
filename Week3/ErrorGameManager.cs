9825d3a705ae815341d47b7e945fd9ab3a701921
SoHyeon Kim <happythgus89@gmail.com>
2022-02-27 15:50:30 +0900

    Create ErrorGameManager.cs

---
+++ Week3/ErrorGameManager.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // game manager의 역할: 점수 관리, 스테이지 이동을 관리
+
+    public int totalPoint;
+    public int stagePoint;
+    public int stageIndex;
+    public int health;
+    public PlayerMove player;
+    public GameObject[] Stages;
+
+    public Image[] UIhealth;
+    public Text UIPoint;
+    public Text UIStage;
+    public GameObject UIRestartBtn;
+
+    void Update()
+    {
+        UIPoint.text = (totalPoint + stagePoint).ToString();
+    }
+
+
+    public void NextStage()
+    {
+        //Change Stage
+        if(stageIndex < Stages.Length - 1)
+        {
+            Stages[stageIndex].SetActive(false);
+            stageIndex++;
+            Stages[stageIndex].SetActive(true);
+            PlayerReposition();
+
+            UIStage.text = "STAGE" + (stageIndex + 1);
+        }
+        else // Game Clear
+        {
+            //Player Control Lock
+            Time.timeScale = 0;
+
+            //Restart Button UI
+            Text btnText = UIRestartBtn.GetComponentInChildren<Text>();
+            btnText.text = "Clear! :)";
+            ViewBtn();
+        }
+
+        //Calculate Point
+        totalPoint += stagePoint;
+        stagePoint = 0;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {            
+            //Player Reposition
+            if(health > 1)
+            {
+                PlayerReposition();
+            }
+
+            //Health Down
+            HealthDown();
+
+        }
+    }
+
+    public void HealthDown()
+    {
+        if(health > 1)
+        {
+            health--;
+            UIhealth[health].color = new Color(1, 0, 0, 0.4f);
+        }
+        else
+        {
+            //All Health UI Off
+            UIhealth[0].color = new Color(1, 0, 0, 0.4f);
+                        
+            //Player Die Effect
+            player.OnDie();
+            
+            //Retry Button UI 
+            UIRestartBtn.SetActive(true);
+        }
+    }
+
+    void PlayerReposition()
+    {
+        player.transform.position = new Vector3(0, 0, -1);
+        player.VelocityZero();
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+    void ViewBtn()
+    {
+        UIRestartBtn.SetActive(true);
+    }
+
+
+}
