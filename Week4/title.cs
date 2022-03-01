8740d158cd5ef61fcd4cc5446c199660abd93214
SoHyeon Kim <happythgus89@gmail.com>
2022-03-01 15:44:06 +0900

    title start

---
+++ Week4/title.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scenemanager : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        Application.LoadLevel("Stage1"); 
+    }
+
+}
+
+ 
